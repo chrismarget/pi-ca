1bfd1ac363821e5c4db54c0bd37b257ccfc45486
chris marget <5470536+chrismarget@users.noreply.github.com>
2020-01-08 19:07:11 -0500

    start scripts for rtc

--- copy_to_sd_boot/go-init.d/11_rtc.sh
+++ copy_to_sd_boot/go-init.d/11_rtc.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
+# These take forever. It's easier to just leave the service installed
+# but disabled. Maybe add these commands to soemthing other than the
+# first (go-init) boot.
+#apt-get -y remove fake-hwclock
+#apt-get -y purge fake-hwclock
+#update-rc.d -f fake-hwclock remove
+
+rm /etc/systemd/system/sysinit.target.wants/fake-hwclock.service
+mv /etc/rcS.d/S01fake-hwclock /etc/rcS.d/K01fake-hwclock
 echo "dtoverlay=i2c-rtc,ds3231" >> /boot/config.txt
 patch /lib/udev/hwclock-set < $(basename $0)/patch.d/hwclock-set.patch
