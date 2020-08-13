ac27dc58714cee2f621293d2a1ebeb08681b7277
chris marget <5470536+chrismarget@users.noreply.github.com>
2020-08-13 12:27:06 -0400

    rtc needs to disable systemd-timesyncd

--- copy_to_sd_boot/go-init.d/11_rtc.sh
+++ copy_to_sd_boot/go-init.d/11_rtc.sh
@@ -3,12 +3,25 @@
 # These take forever. It's easier to just leave the service installed
 # but disabled. Maybe add these commands to soemthing other than the
 # first (go-init) boot.
-#apt-get -y remove fake-hwclock
-#apt-get -y purge fake-hwclock
-#update-rc.d -f fake-hwclock remove
+echo "#########################"
+echo "Removing fake-hwclock"
+apt-get -y remove fake-hwclock
 
-rm /etc/systemd/system/sysinit.target.wants/fake-hwclock.service
-mv /etc/rcS.d/S01fake-hwclock /etc/rcS.d/K01fake-hwclock
-rm /etc/cron.hourly/fake-hwclock
+echo "#########################"
+echo "purging fake-hwclock"
+apt-get -y purge fake-hwclock
+
+echo "#########################"
+echo "Calling update-rc.d -f fake-hwclock remove"
+update-rc.d -f fake-hwclock remove
+
+echo "#########################"
+echo "Disabling systemd-timesyncd"
+systemctl disable systemd-timesyncd
+
+echo "#########################"
+echo "Enabling RTC hardware"
 echo "dtoverlay=i2c-rtc,ds3231" >> /boot/config.txt
 echo "i2c_dev" >> /etc/modules
+
+echo "#########################"
