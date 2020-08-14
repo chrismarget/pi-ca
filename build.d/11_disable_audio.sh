b97148ad31ea7fec7897704f0c407b4c1780da03
cmarget <cmarget@mutualink.net>
2020-08-14 14:20:43 -0400

    improved checks in disable_audio

--- build.d/11_disable_audio.sh
+++ build.d/11_disable_audio.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-if [ -e "$BOOT_MNT" ]
-then
-  echo disabling audio driver
-  sed -i .bak 's/dtparam=audio=on/dtparam=audio=off/' $BOOT_MNT/config.txt
-  rm $BOOT_MNT/config.txt.bak
-fi
+. $(dirname $0)/functions
+[ -d "$BOOT_MNT" ] || error "$BOOT_MNT does not exist"
+
+BOOT_CFG="${BOOT_MNT}/config.txt"
+[ -f $BOOT_CFG ] || error "$BOOT_CFG does not exist"
+
+sed -i .bak 's/dtparam=audio=on/dtparam=audio=off/' $BOOT_CFG && rm ${BOOT_CFG}.bak
