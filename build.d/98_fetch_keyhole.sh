d3c1368d0dee872313e3e12946378e5df7000080
cmarget <cmarget@mutualink.net>
2020-08-25 18:09:44 -0400

    rename yupipica -> keyhole

--- build.d/98_fetch_keyhole.sh
+++ build.d/98_fetch_keyhole.sh
@@ -10,7 +10,7 @@
 [ -n "$P3_LABEL" ] || error "P3_LABEL unset"
 
 # we store the application in cache_dir
-git_url="https://github.com/chrismarget/yupypica.git"
+git_url="https://github.com/chrismarget/keyhole.git"
 project_name="$(echo $git_url | sed -e 's:^.*/\(.*\).git$:\1:')"
 tar_file="${project_name}.tar"
 cache_dir="${PROJECT_DIR}/.build.cache"
@@ -18,13 +18,13 @@ cache_dir="${PROJECT_DIR}/.build.cache"
 # fetch/update the application
 if [ -d "$cache_dir/$project_name" ]
 then
-  git -C "$cache_dir/$project_name" pull --recurse-submodules $git_url
+  git -C "$cache_dir/$project_name" pull --recurse-submodules "$git_url"
 else
-  git -C "$cache_dir" clone --recursive $git_url
+  git -C "$cache_dir" clone --recursive --branch master "$git_url"
 fi
 
 # roll up the application into the SD card
-tar cf $P3_MNT/${project_name}.tar -C "${cache_dir}" --exclude ".git*" "${project_name}"
+tar cf $P3_MNT/${project_name}.tar -C "${cache_dir}" "${project_name}"
 
 # leave an install script on the SD card
 rc_local_d="${BOOT_MNT}/rc.local.d"
@@ -38,7 +38,7 @@ echo "rm \$0" >> $install_script
 
 # leave a startup script on the SD card
 start_script="${rc_local_d}/99_${project_name}.sh"
-start_script="${BOOT_MNT}/rc.local.d/99_yupypica.sh"
+start_script="${BOOT_MNT}/rc.local.d/99_keyhole.sh"
 echo "#!/bin/sh" > $start_script
 echo "chvt 11" >> $start_script
-echo "/usr/local/yupypica/bin/yupypica > /dev/tty11 < /dev/tty11 &" >> $start_script
+echo "/usr/local/keyhole/bin/keyhole > /dev/tty11 < /dev/tty11 &" >> $start_script
